63a4032b5ba6b8c6238b347ccfca82cb0669736c
Daniel Macicasan <danimacicasan@gmail.com>
2021-02-25 20:53:40 -0500

    #10 uncommenting JSONWebToken

--- polaroid/polaroid/settings.py
+++ polaroid/polaroid/settings.py
@@ -57,7 +57,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
