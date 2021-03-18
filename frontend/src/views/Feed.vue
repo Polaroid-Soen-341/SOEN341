be985cd7036a8fefd45fc046d0dca40a0a4bf0cb
Daniel Macicasan <danimacicasan@gmail.com>
2021-03-17 23:05:15 -0400

    #7 adding comment section under posts

---
+++ frontend/src/views/Feed.vue
@@ -0,0 +1,95 @@
+<template>
+    <v-container grid-list-md>
+      <v-layout row wrap align-center justify-center fill-height>
+        <v-flex xs12 sm8 lg5 md6>
+                <v-card rounded outlined style="max-width: 800px">
+                    <v-card-title>
+                        Welcome to Polaroid, {{ currentUser }}!
+                    </v-card-title>
+                    <!-- <div class="px-3">
+                      <v-text-field class="questrial no-top-padding" background-color="grey lighten-3" height="44px" rounded placeholder="Search a user"/>
+                    </div> -->
+                    <div v-for="n in 9"
+                    :key="n">
+                    <v-divider class="mt-1 mb-1"></v-divider>
+                    <v-card-title>Username</v-card-title>
+                    <div class="px-4">
+                      <v-img
+                        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                        aspect-ratio="1"
+                        class="grey lighten-2">
+                        <template v-slot:placeholder>
+                          <v-row
+                            class="fill-height ma-0"
+                            align="center"
+                            justify="center">
+                            <v-progress-circular
+                              indeterminate
+                              color="grey lighten-5"
+                            ></v-progress-circular>
+                          </v-row>
+                        </template>
+                      </v-img>
+                    </div>
+                    <div class="px-4">
+                      <v-btn icon
+                             :color="likeBtn? 'pink' : ''"
+                             large
+                             @click="likeClicked()">
+                        <v-icon>mdi-heart</v-icon>
+                      </v-btn>
+                      <v-btn icon
+                             color="black"
+                             large>
+                        <v-icon>mdi-comment-outline</v-icon>
+                      </v-btn>
+                    </div>
+                    <div class="px-4">
+                      20 Likes
+                    </div>
+                    <div class="px-4">
+                      Username - Comment
+                    </div>
+                    <div class="px-4">
+                      <v-textarea
+                        label="Add comment..."
+                        auto-grow
+                        filled
+                        rounded
+                        rows="1"
+                        row-height="15"
+                      ></v-textarea>
+                    </div>
+                    </div>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: "Feed",
+  mounted() {
+    this.loadUser()
+  },
+  data: () => ({
+    currentUser: '',
+    userFirstname: '',
+    userLastname: '',
+    likeBtn: false,
+  }),
+  methods: {
+    loadUser(){
+      this.currentUser = this.$session.get('current_user')
+    },
+    likeClicked() {
+      this.likeBtn = !this.likeBtn
+    }
+  }
+}
+</script>
+
+<style>
+</style>
