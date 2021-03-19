bb49f828bef35dfb5ed99a9f16acb261e1002520
TheLastBilly <jtmonegro@gmail.com>
2021-03-18 21:19:29 -0400

    #5: fixed urls, removed Picture, updated views, fixed media retrieve

--- polaroid/content/models.py
+++ polaroid/content/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 import uuid
 
 class Post(models.Model):
@@ -7,6 +8,7 @@ class Post(models.Model):
     description     = models.TextField(null=True)
     date            = models.DateTimeField(auto_now_add=True)
     owner           = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE, null=False)
+    picture         = models.ImageField(null=True, blank=True, upload_to="pictures/")
     id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
@@ -21,14 +23,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
-
-class Picture(models.Model):
-    title           = models.CharField(max_length=100, null=True)
-    picture         = models.ImageField(null=True)
-    date            = models.DateTimeField(auto_now_add=True)
-    post            = models.ForeignKey(Post, on_delete=models.CASCADE, null=False, related_name="pictures")
-    owner           = models.ForeignKey(User, related_name='pictures', on_delete=models.CASCADE, null=False)
-    id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
-    def __str__(self):
-        return self.title
--- polaroid/polaroid/urls.py
+++ polaroid/polaroid/urls.py
@@ -20,9 +20,12 @@ from rest_framework import routers, serializers, viewsets
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework.authtoken import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('api_auth.urls')),
     path('content/', include('content.urls')),
     path('api-token-auth/', obtain_jwt_token)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
