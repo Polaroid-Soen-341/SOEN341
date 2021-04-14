d6b8ad4d793639cc6e54566ca02dd42fe3a0aefe
Nathan <nathanmf24@gmail.com>
2021-04-14 14:16:26 -0400

    #45 made additional tests

--- polaroid/api_auth/tests.py
+++ polaroid/api_auth/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+class UserCreationTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user("admin","admin@admin.com","stronpass")
+
+    def test_user_were_created(self):
+        """Test that user was created"""
+        owner = User.objects.get(name)
+        self.assertEqual(owner.is_staff,False,"staff False")
+        self.assertEqual(owner.is_superuser,False,"Super User False")
+        self.assertEqual(owner.username,"admin","username match")
+        self.assertEqual(owner.email,"admin@admin.com","email match")
+        self.assertEqual(owner.password,"strongpass","pass match")
+
+class SuperUserCreationTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_superuser("admin","admin@admin.com","stronpass")
+
+    def test_user_were_created(self):
+        """Test that user was created"""
+        owner = User.objects.get(name)
+        self.assertEqual(owner.is_staff,True,"staff true")
+        self.assertEqual(owner.is_superuser,True,"Super User true")
+        self.assertEqual(owner.username,"admin","username match")
+        self.assertEqual(owner.email,"admin@admin.com","email match")
+        self.assertEqual(owner.password,"strongpass","pass match")
--- polaroid/content/tests.py
+++ polaroid/content/tests.py
@@ -3,6 +3,7 @@ from api_auth.models import User
 from .models import *
 
 # Create your tests here.
+
 class PostCreationTestCase(TestCase):
     def setUp(self):
         owner = User.objects.create_user("admin", "adminadmin.com", "pass")
@@ -12,3 +13,33 @@ class PostCreationTestCase(TestCase):
         """Test if posts were created correctly"""
         post = Post.objects.get(title="Post 1")
         self.assertEqual(post.description, "whatever", "Post 1 Created")
+
+    def test_post_title_limit(self):
+        #limit = 100 char
+        owner = User.objects.create_user("admin", "adminadmin.com", "pass")
+        toolong= 'x'*120
+        post = Post.objects.create(title=toolong, description="whatever", owner=owner)
+
+
+
+        
+class CommentCreationTestCase(TestCase):
+    def setUp(self):
+        self.owner = User.objects.create_user("admin", "adminadmin.com", "pass")
+        post=Post.objects.create(title="Post 1", description="whatever", owner=self.owner)
+        Comment.objects.create(content="Lorem Ipsum",post=post,owner=self.owner)
+        
+    def test_comment_was_created(self):
+        comment = Comment.objects.get(post= Post.objects.get(title="Post 1"))
+        self.assertEqual(comment.content,"Lorem Ipsum", "comment created")
+
+# class FollwingTestCase(TestCase):
+#     def setUp(self):
+#         user1= User.objects.create_user("user1", "user1@user.com", "userpass")
+#         user2= User.objects.create_user("user2", "user2@user.com", "userpass")
+
+#     def Test_use_can_follow(self):
+
+#         #make user1 follow user2 them follow
+#         #assert user1 follows user2
+#         #assert user2 does not follow user1
